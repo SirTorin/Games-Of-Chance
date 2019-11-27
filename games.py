53f85f0ffb82e238f82842cb52e5699f6c4e33be
SirTorin <torinadot@comcast.net>
2019-11-27 15:51:14 -0600

    cho han finished

--- games.py
+++ games.py
@@ -7,77 +7,169 @@ money = 100
 def coin_flip(bet, guess):
     global money
 
-    not_int_bet = False
-    bet_too_big = False
-    try_it2 = False
-    try_it3 = False
+    bet_not_int = False
+    bet_over = False
+    bet_under = False
     bad_guess = False
 
     try:
         bet = int(bet)
     except ValueError:
-        not_int_bet = True
+        bet_not_int = True
 
     num = random.randint(1, 2)
 
-    if not_int_bet == False:
-        try:
-            if type(money) == type(bet):
-                try_it3 = True
-        except TypeError:
-            ot_int_bet = True
-        else:
-            if money < int(bet):
-                bet_too_big = True
-            else:
-                try_it2 = True
-    if guess.lower() == "heads" or guess.lower() == "tails":
+    if bet_not_int == False:
+        if money < bet:
+            bet_over = True
+
+        elif bet < 1:
+            bet_under = True
+
+
+    if guess.lower() == "heads" or guess.lower() == "h" or guess.lower() == "tails" or guess.lower() == "t":
         bad_guess = False
     else:
         bad_guess = True
 
-    if try_it2 == True and try_it3 == True and not_int_bet == False and not_int_bet == False:
+    if bet_under == False and bad_guess == False and bet_not_int == False and bet_over == False:
         if num == 1:
             side = "Heads"
         else:
             side = "Tails"
 
-        if guess.lower() == side.lower():
+        short_side = side[:1]
+
+        if guess.lower() == side.lower() or guess.lower() == short_side.lower():
             money += int(bet)
-            return "And the flip was..", side.capitalize() + "! You win", "+$" + str(bet), "   balance is now", "$" + str(money)
+            return "And the flip was..." + side.capitalize() + "! You win" + " +$" + str(bet) + ". balance is now " + "$" + str(money)
 
-        elif guess.lower() == "heads" or guess.lower() == "tails":
+        else:
             money -= int(bet)
-            return "And the flip was..." + side.capitalize() + "! You lose" + " -$" + str(bet) + " balance is now", "$" + str(money)
+            return "And the flip was..." + side.capitalize() + "! You lose" + " -$" + str(bet) + ". balance is now " + "$" + str(money)
 
-    if bad_guess == True and not_int_bet == True:
-        return "Bet is not a number and guess not valid"
+    if bad_guess == True and bet_not_int == True:
+        return "ERROR. Bet must be a number, and guess must be heads or tails"
 
-    elif bad_guess == True and bet_too_big == True:
-        return "Not enough money and guess not valid"
+    elif bad_guess == True and bet_over == True:
+        return "ERROR. Bet over, and guess must be heads or tails"
 
-    elif not_int_bet == False and bet_too_big == False:
-        return "Guess not valid"
+    elif bad_guess == True and bet_under == True:
+        return "ERROR. Bet under, and guess must be heads or tails"
 
-    elif not_int_bet == True and bet_too_big == False:
-        return "Bet is not a number."
+    elif bad_guess == True:
+        return "ERROR. Guess not valid"
 
-    elif not_int_bet == False and bet_too_big == True:
-        return "You do not have enough money."
+    elif bet_not_int == True:
+        return "ERROR. Bet is not a number."
+
+    elif bet_over == True:
+        return "ERROR Not enough money."
+    elif bet_under == True:
+        return "ERROR. bet must be at least $1"
 
 
 def cho_han(bet, guess):
 
+    global money
+
     num1 = random.randint(1, 6)
     num2 = random.randint(1, 6)
     num = num1 + num2
+    if num % 2 == 0:
+        even_or_odd = "Even"
+    else:
+        even_or_odd = "Odd"
 
-    not_int_bet = False
-    bet_too_big = False
-    not_int_guess = False
-    guess_too_big = False
+    bet_not_int = False
+    bet_over = False
+    bet_under = False
+    guess_not_int = False
+    guess_over = False
+    guess_under = False
+
+    try:
+        bet = int(bet)
+    except ValueError:
+        bet_not_int = True
+
+    if bet_not_int == False:
+        if money < bet:
+            bet_over = True
+
+        elif bet < 1:
+            bet_under = True
+    try:
+        guess = int(guess)
+    except ValueError:
+        guess_not_int = True
+
+    if guess_not_int == False:
+        if guess > 12:
+            guess_over = True
+
+        elif guess < 2:
+            guess_under = True
+
+
+    if bet_not_int == False  and bet_under == False and bet_over == False and guess_not_int == False  and guess_under == False and guess_over == False:
+        if (num % 2) == (guess % 2):
+            money += int(bet)
+            return "And the roll was..." + even_or_odd + "! (" + str(num1) + " + " + str(num2) + " total: " + str(num) + ") You win" + " +$" + str(bet) + ". balance is now " + "$" + str(money)
+        else:
+            money -= int(bet)
+            return "And the roll was..." + even_or_odd + "! (" + str(num1) + " + " + str(num2) + " total: " + str(num) + ") You lose" + " -$" + str(bet) + ". balance is now " + "$" + str(money)
+
+
+
+    if bet_not_int == True and guess_not_int == True:
+        return "ERROR. Bet must be a number, and guess must be a number."
+
+    elif bet_not_int == True and guess_over == True:
+        return "ERROR. Bet must be a number, and guess must be 2 to 12."
+
+    elif bet_not_int == True and guess_under == True:
+        return "ERROR. bet must be a number, and guess must be 2 to 12."
+
+
+    elif bet_over == True and guess_not_int == True:
+        return "ERROR. Bet over, and guess must be a number."
+
+    elif bet_over == True and guess_over == True:
+        return "ERROR. Bet over, and guess must be 2 to 12."
+
+    elif bet_over == True and guess_under == True:
+        return "ERROR. Bet over, and guess must be 2 to 12."
+
+
+    elif bet_under == True and guess_not_int == True:
+        return "ERROR. Bet under, and guess must be a number."
+
+    elif bet_under == True and guess_over == True:
+        return "ERROR. Bet under, and guess must be 2 to 12."
+
+    elif bet_under == True and guess_under == True:
+        return "ERROR. Bet under, and guess must be 2 to 12."
+
+
+    elif bet_not_int == True:
+        return "ERROR. bet must be a number."
+    elif bet_over == True:
+        return "ERROR. Not enough money."
+    elif bet_under == True:
+        return "ERROR. Bet must be at least $1."
+
+    elif guess_not_int == True:
+        return "ERROR. Guess must be a number."
+    elif guess_over == True:
+        return "ERROR. Guess must be 2 to 12."
+    elif guess_under == True:
+        return "ERROR. Guess must be 2 to 12."
+
+
 
 
 # Call your game of chance functions here
 
-print(coin_flip(input("Bet "), input("Guess ")))
+#print(coin_flip(input("Coin-Flip      Bet "), input("Coin-Flip    Guess ")))
+print(cho_han(input("Cho-Han      Bet "), input("Cho-Han    Guess ")))
